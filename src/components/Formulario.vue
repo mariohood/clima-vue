41518e96fe31c1c140c0d00f7a308a5247409374
mariohood <manhattan199@hotmail.com>
2025-02-25 13:04:45 -0300

    168 Validando Formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,4 +1,14 @@
 <script setup>
+  import { reactive, ref } from 'vue';
+  import Alerta from './Alerta.vue';
+
+  const busqueda = reactive({
+    ciudad: '',
+    pais: ''
+  })
+
+  const error = ref('')
+
   const paises = [
     { codigo: 'US', nombre: 'Estados Unidos' },
     { codigo: 'MX', nombre: 'México' },
@@ -6,14 +16,32 @@
     { codigo: 'CO', nombre: 'Colombia' },
     { codigo: 'CR', nombre: 'Costa Rica' },
     { codigo: 'ES', nombre: 'España' },
-    { codigo: 'PE', nombre: 'Perú' }
+    { codigo: 'PE', nombre: 'Perú' },
+    { codigo: 'VE', nombre: 'Venezuela' },
+    { codigo: 'CL', nombre: 'Chile' },
+    { codigo: 'EC', nombre: 'Ecuador' },
+    { codigo: 'BR', nombre: 'Brasil' }
 ]
+
+const consultarClima = () => {
+  if(Object.values(busqueda).includes('')){
+    error.value = 'Todos los campos son obligatorios'
+    return
+  }
+
+  error.value = ''
+}
+
+
 </script>
 
 <template>
   <form
   class="formulario"
+  @submit.prevent="consultarClima"
   >
+    <Alerta v-if="error" >{{ error }}</Alerta>
+
     <div class="campo">
 
       <label for="ciudad">Ciudad</label>
@@ -21,17 +49,23 @@
       type="text"
       id="ciudad"
       placeholder="Ciudad"
+      v-model="busqueda.ciudad"
       >
     </div>
 
     <div class="campo">
 
-      <label for="ciudad">País</label>
-      <select>
+      <label for="pais">País</label>
+      <select
+        id="pais"
+        v-model="busqueda.pais"
+      >
         <option value="">-- Seleccione un país --</option>
         <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
       </select>
     </div>
+
+    <input type="submit" value="Consultar Clima" />
   </form>
 </template>
 
