4eb5dd60b9167f691d8d796f3b2d3ed24109f2e3
mariohood <manhattan199@hotmail.com>
2025-02-24 21:27:23 -0300

    Projeto 7 (166) criando formulario principal

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import Formulario from './components/Formulario.vue'
+</script>
+
+<template>
+  <h1 class="titulo">Buscador de Clima</h1>
+
+  <div class="contenedor buscador-clima">
+    <Formulario />
+  </div>
+ 
+</template>
+
+
---
+++ src/components/Formulario.vue
@@ -0,0 +1,39 @@
+<script setup>
+  const paises = [
+    { codigo: 'US', nombre: 'Estados Unidos' },
+    { codigo: 'MX', nombre: 'México' },
+    { codigo: 'AR', nombre: 'Argentina' },
+    { codigo: 'CO', nombre: 'Colombia' },
+    { codigo: 'CR', nombre: 'Costa Rica' },
+    { codigo: 'ES', nombre: 'España' },
+    { codigo: 'PE', nombre: 'Perú' }
+]
+</script>
+
+<template>
+  <form
+  class="formulario"
+  >
+    <div class="campo">
+
+      <label for="ciudad">Ciudad</label>
+      <input
+      type="text"
+      id="ciudad"
+      placeholder="Ciudad"
+      >
+    </div>
+
+    <div class="campo">
+
+      <label for="ciudad">País</label>
+      <select>
+        <option value="">-- Seleccione un país --</option>
+        <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
+      </select>
+    </div>
+  </form>
+</template>
+
+
+
